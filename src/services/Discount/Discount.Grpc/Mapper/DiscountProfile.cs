716c0ff5d2e22c7e5c1e657d6f119e4db66bb272
Your Name <you@example.com>
2021-12-28 01:41:31 +0000

    integrate grpc

--- src/services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
+++ src/services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -1,6 +1,13 @@
-﻿namespace Discount.Grpc.Mapper;
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
 
-public class DiscountProfile
+namespace Discount.Grpc.Mapper;
+
+public class DiscountProfile : Profile
 {
-    
+    public DiscountProfile()
+    {
+        CreateMap<Coupon, CouponModel>().ReverseMap();
+    }
 }
